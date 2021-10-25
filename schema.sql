430af4248c6a36a51035c7b82f0952b02d1e19d8
Deny7676yar <komarov7676@yandex.ru>
2021-10-25 22:37:26 +0300

    lesson2 data

--- schema.sql
+++ schema.sql
@@ -7,24 +7,47 @@
 --8. Схема
 
 CREATE TABLE stores (
-			id            serial PRIMARY KEY,
-			name          varchar(200) NOT NULL,
-			addr          text NOT NULL,
-            street        text NOT NULL,
-			name_product  varchar(100),
-			
+			id            INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+			name          VARCHAR(200) NOT NULL,
+			city          VARCHAR(200) NOT NULL,
+            street        VARCHAR(200) NOT NULL,
+            numb_street   INT NOT NULL
 		);
 
 CREATE TABLE products (
-			id          serial PRIMARY KEY,
-			name        varchar(200) NOT NULL,
-			price       integer
+			id          INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+			name        VARCHAR(200) NOT NULL,
+			price       MONEY CONSTRAINT positive_price CHECK (price::numeric > 0)
 		);
 
 CREATE TABLE quantity (
-			store_id          integer,
-			product_id        integer,
-			quantity		  integer,
+			store_id          INT,
+			product_id        INT,
+			quantity		  INT,
 			FOREIGN KEY (store_id) REFERENCES stores (id),  
 			FOREIGN KEY (product_id) REFERENCES products (id)
 		);
+
+CREATE TABLE users(
+            id            INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            first_name    VARCHAR(200) NOT NULL,
+            last_name     VARCHAR(200) NOT NULL,
+            city_user          VARCHAR(200) NOT NULL,
+            street_user        VARCHAR(200) NOT NULL,
+            numb_street_user   INT NOT NULL
+        );
+
+
+--CREATE DOMAIN domain_email AS citext
+--CHECK(
+-- VALUE ~ '^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$'
+--);
+
+CREATE TABLE contacts_user (
+            user_id            INT,
+            contact_id         uuid DEFAULT uuid_generate_v4 (),
+            email              VARCHAR(200) NOT NULL,
+            phone              VARCHAR(100),
+            PRIMARY KEY (contact_id),
+            FOREIGN KEY (user_id) REFERENCES users(id)
+);
