5ba163df4ed84b0ee513e0f0d3f090c997394ff7
Deny7676yar <komarov7676@yandex.ru>
2021-10-25 22:25:13 +0300

    lesson2 data

---
+++ data.sql
@@ -0,0 +1,10 @@
+INSERT INTO stores(name, city, street, numb_street) VALUES ('Rigla', 'Yaroslavl', 'Belinskogo', 26);
+
+INSERT INTO products(name, price) VALUES ('ТераФлю', 200);
+
+INSERT INTO quantity(store_id, product_id, quantity) VALUES (1, 1, 32);
+
+INSERT INTO users(first_name, last_name, city_user, street_user, numb_street_user) VALUES ('Denis', 'Komarov', 'Yaroslavl', 'Kosmonavtov', 18);
+
+--INSERT INTO contacts_user(user_id, contact_id, email, phone) VALUES (1, 1, "komarov7676@yandex.ru", "25-12-12");
+
